9140c4fcc2f2ad8ccb87a9b898ab580478b3c834
king <kelvinozak0@gmail.com>
2026-02-07 22:28:19 +0100

    first commit

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -28,7 +28,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
       if (savedTheme) {
       setTheme(savedTheme)
     } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      setTheme('')
+      setTheme('light')
     }
 
 
