aeb596bc86d4a624aa4a7c75b8cb34b517e64518
king <kelvinozak0@gmail.com>
2026-02-11 23:53:24 +0100

    Initial commit  paynest demo site

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -144,6 +144,7 @@ export default function DashboardPage() {
               <TableRow>
                 <TableHead>Task</TableHead>
                 <TableHead>Assigned To</TableHead>
+                <TableHead>Assigned From</TableHead>
                 <TableHead>Status</TableHead>
                 <TableHead>Deadline</TableHead>
               </TableRow>
