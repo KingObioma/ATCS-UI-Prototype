475f9c7a3cec50b767c983176b44d2db87bce06d
king <kelvinozak0@gmail.com>
2026-02-12 23:57:02 +0100

    Initial commit  ATCS demo site

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -14,7 +14,7 @@ export default function RegisterPage() {
   const [formData, setFormData] = useState({
     name: 'John Doe',
     email: 'john@atcs.io',
-    email: 'john@atcs.io',
+    email: 'john@as.io',
     password: 'admin123',
     confirmPassword: 'admin123',
   })
