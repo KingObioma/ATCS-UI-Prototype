f596d604deb56a2d7690115c66982e320d5e6993
king <kelvinozak0@gmail.com>
2026-03-04 22:27:29 +0100

    tech stack Modifications

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -14,7 +14,7 @@ export default function RegisterPage() {
   const [formData, setFormData] = useState({
     name: 'John Doe',
     email: 'john@atcs.io',
-    password: 'admin143',
+    password: 'admin123',
     confirmPassword: 'admin123',
   })
 
